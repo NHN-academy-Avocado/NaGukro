4a33e347f8c1c3587bd462667bce0e1299d93232
nayoseb <nayoseb1018@gmail.com>
2023-11-21 09:11:48 +0900

    chapter11 Exam05

---
+++ JavaFXEdition/src/main/java/org/chapter/chapter11/Java_Socket_통신/Exam05.java
@@ -0,0 +1,22 @@
+package org.chapter.chapter11.Java_Socket_통신;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Exam05 {
+    public static void main(String[] args) {
+        int port = 1234;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            Socket socket = serverSocket.accept();
+
+            socket.getOutputStream().write("Hello!".getBytes());
+            socket.getOutputStream().flush();
+
+            socket.close();
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+}
